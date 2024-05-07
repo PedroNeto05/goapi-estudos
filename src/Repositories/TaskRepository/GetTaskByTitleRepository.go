c994d44168d43677f20dd5f95d751d39c85a4563
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-06 22:30:47 -0300

    feat: finish get task bu title repository

--- src/Repositories/TaskRepository/GetTaskByTitleRepository.go
+++ src/Repositories/TaskRepository/GetTaskByTitleRepository.go
@@ -2,11 +2,15 @@ package taskrepository
 
 import (
 	models "goApi/src/Models"
-
-	"gorm.io/gorm"
 )
 
-func GetTaskByTitleRepository(title string) *gorm.DB {
-	task := db.First(&models.Task{})
-	return task
+func GetTaskByTitleRepository(title string) (*models.Task, error) {
+	var task models.Task
+
+	err := db.Where("title = ?", title).First(&task).Error
+
+	if err != nil {
+		return &task, err
+	}
+	return &task, nil
 }
