5317927f5a78375567c3c64f7a5b88e728c86abc
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-09 16:45:45 -0300

    refactor: change pointer

--- src/api/repositories/task/get_task_by_title_repository.go
+++ src/api/repositories/task/get_task_by_title_repository.go
@@ -3,12 +3,12 @@ package taskrepository
 import "goApi/db/models"
 
 func GetTaskByTitleRepository(title string) (*models.Task, error) {
-	var task models.Task
+	var task *models.Task
 
-	err := db.Where("title = ?", title).First(&task).Error
+	err := db.Where("title = ?", title).First(task).Error
 
 	if err != nil {
-		return &task, err
+		return task, err
 	}
-	return &task, nil
+	return task, nil
 }
