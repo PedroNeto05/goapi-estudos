9ed25617d148593a3ffb7cdb71408fc37f47d0ca
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-05-09 22:56:08 -0300

    refactor: change logic

--- src/api/repositories/task/get_task_by_title_repository.go
+++ src/api/repositories/task/get_task_by_title_repository.go
@@ -1,17 +1,15 @@
 package taskrepository
 
 import (
-	"errors"
 	"goApi/db/models"
-
-	"gorm.io/gorm"
 )
 
 func GetTaskByTitleRepository(title string) (*models.Task, error) {
 	var task models.Task
+
 	err := db.Where("title = ?", title).First(&task).Error
 
-	if errors.Is(err, gorm.ErrRecordNotFound) || err != nil {
+	if err != nil {
 		return nil, err
 	}
 
